0c67b1ad5901b76a1aa4f6c5b9c7276083e0a208
Nat3z <66748576+Nat3z@users.noreply.github.com>
2026-02-14 21:35:33 -0800

    fix(electron): update externalizeDepsPlugin to exclude 'webtorrent' in addition to 'ogi-addon'

--- application/electron.vite.config.ts
+++ application/electron.vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin({ exclude: ['ogi-addon'] })],
+    plugins: [externalizeDepsPlugin({ exclude: ['ogi-addon', 'webtorrent'] })],
     build: {
       rollupOptions: {
         input: {

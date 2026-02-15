b82a6707aadcb603a1af1ce45a43c1e6200a156b
Nat3z <66748576+Nat3z@users.noreply.github.com>
2026-02-14 21:45:34 -0800

    fix: fixed error with modules node

--- application/electron.vite.config.ts
+++ application/electron.vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin({ exclude: ['ogi-addon', 'webtorrent'] })],
+    plugins: [externalizeDepsPlugin({ exclude: ['ogi-addon'] })],
     build: {
       rollupOptions: {
         input: {

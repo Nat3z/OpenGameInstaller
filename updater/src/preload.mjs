c35fbb5f29792a05e254a7e06e294834930e4085
nat-openclaw <258557496+nat-openclaw@users.noreply.github.com>
2026-06-02 11:15:31 -0700

    feat(updater): add bleeding edge commit channel

--- updater/src/preload.mjs
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-ipcRenderer.on('text', (event, text, progress, max, subtext) => {
-  document.dispatchEvent(
-    new CustomEvent('text', { detail: { text, progress, max, subtext } })
-  );
-});
